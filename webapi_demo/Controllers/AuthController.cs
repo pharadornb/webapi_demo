9cecd0d80fdf3707a67219502e4d4b0a56a10f91
pharadornbr <pharadorn.b@softtechmedcare.com>
2024-11-12 17:54:02 +0700

    Initial commit

---
+++ webapi_demo/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi_demo.Models;
+
+namespace webapi_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        [EnableCors("AllowSpecificOrigin")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (request.Username == "admin" && request.Password == "admin")
+            {
+                return Ok(new { token = "Welcome Mr.Bom+" });
+            }
+            return Unauthorized();
+        }
+    }
+}
